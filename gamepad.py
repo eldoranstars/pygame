f8213150a67090601bd1328562951490cbceadab
eldoranstars <eldoranstars@gmail.com>
2022-10-03 21:53:55 +0300

    addPause

--- gamepad.py
+++ gamepad.py
@@ -5,13 +5,9 @@ BLUE = pygame.Color(0, 200, 255)
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 720
 
-def tprint(screen, textString, x, y):
-    # https://www.pygame.org/docs/ref/font.html#pygame.font.Font
-    # Font(filename|object|pathlib.Path, size) -> Font
-    # https://www.pygame.org/docs/ref/font.html#pygame.font.Font.render
-    # render(text, antialias, color, background=None) -> Surface
-    # https://www.pygame.org/docs/ref/surface.html?highlight=blit#pygame.Surface.blit
-    # blit(source, dest, area=None, special_flags=0) -> Rect
+def screen_text(screen, textString, x, y):
+    # pygame provides no way to directly draw text on an existing Surface: 
+    # instead you must use Font.render() to create an image (Surface) of the text, then blit this image onto another Surface.
     textBitmap = pygame.font.Font(None, 20).render(textString, True, BLACK)
     screen.blit(textBitmap, (x, y))
 
@@ -32,11 +28,23 @@ def player_joystick(player, joystick):
             player['y'] -= player['velocity']
         if joystick.get_hat(0)[1] == -1:
             player['y'] += player['velocity']
+        if joystick.get_button(7) == 1:
+            game_pause(joystick)
+
+def game_pause(joystick):
+    pygame.display.update(screen_text(screen, "BACK to RESUME", SCREEN_WIDTH/2 - 52, 25))
+    pygame.display.update(screen_text(screen, "START to QUIT", SCREEN_WIDTH/2 - 45, 40))
+    pause = True
+    while pause:
+        for event in pygame.event.get():
+            if event.type == pygame.JOYBUTTONDOWN and joystick.get_button(6) == 1:
+                pause = False
+            if event.type == pygame.JOYBUTTONDOWN and joystick.get_button(7) == 1:
+                pygame.quit()
 
 # ----------- Main Program Loop -----------
 pygame.init()
 pygame.display.set_caption("My Game")
-# https://blog.glyph.im/2022/02/a-better-pygame-mainloop.html
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SCALED, vsync=1)
 clock = pygame.time.Clock()
 player1 = {'width' : 40, 'height' : 40, 'velocity' : 5, 'color' : BLUE, 'x' : 50, 'y' : 50}
@@ -58,7 +66,7 @@ while run:
     # First, clear the screen to white. Don't put other drawing commands above this, or they will be erased with this command.
     screen.fill(WHITE)
     joystick_count = pygame.joystick.get_count()
-    tprint(screen, "Number of joysticks : {}".format(joystick_count), SCREEN_WIDTH/2 - 66, 10)
+    screen_text(screen, "Number of joysticks : {}".format(joystick_count), SCREEN_WIDTH/2 - 66, 10)
 
     if joystick_count >= 1:
         pygame.draw.rect(screen, player1['color'], (player1['x'], player1['y'], player1['width'], player1['height']))
@@ -75,7 +83,6 @@ while run:
     # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
     #
 
-    # Go ahead and update the screen with what we've drawn.
     pygame.display.update()
     # Limit frames per second.
     # clock.tick(60)
