492e60ea7be3149d198a2374e4cd8d4bc8a82d57
eldoranstars <eldoranstars@gmail.com>
2022-10-16 23:44:23 +0300

    testCollision

---
+++ game.py
@@ -0,0 +1,19 @@
+import pygame
+import game_functions as gf
+from settings import Settings
+from screen import Screen
+from ship import Ship
+
+pygame.init()
+pygame.display.set_caption("Alien Invasion")
+settings = Settings()
+screen = Screen(settings)
+ship = Ship(screen)
+bullets = []
+aliens = []
+gf.create_fleet(settings, screen, aliens)
+
+while True:
+    gf.check_events()
+    gf.check_keyboard(settings, screen, ship, bullets)
+    gf.update_screen(settings, screen, ship, bullets, aliens)
